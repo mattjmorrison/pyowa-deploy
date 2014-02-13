c24db95d54587153d688b5c44bb7ef95e2af597b
mattjmorrison <mattjmorrison@mattjmorrison.com>
2014-02-12 22:31:36 -0600

    updated repo url to be public url

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 set :application, 'pyowa'
 set :scm, :git
-set :repo_url, 'git@github.com:mattjmorrison/pyowa-deploy.git'
+set :repo_url, 'https://github.com/mattjmorrison/pyowa-deploy.git'
 set :django_project_dir, 'pyowa'
 set :django_settings_dir, 'pyowa/settings'
 set :wsgi_path, 'pyowa'
