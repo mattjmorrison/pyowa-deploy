b262814c37e8e3e72d9c2ba22a0a2734f5994515
mattjmorrison <mattjmorrison@mattjmorrison.com>
2014-02-12 22:33:53 -0600

    tweaking deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 set :application, 'pyowa'
 set :scm, :git
 set :repo_url, 'https://github.com/mattjmorrison/pyowa-deploy.git'
-set :django_project_dir, 'pyowa'
+set :django_project_dir, ''
 set :django_settings_dir, 'pyowa/settings'
 set :wsgi_path, 'pyowa'
 set :pip_requirements, 'requirements.txt'
